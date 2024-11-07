8ed198117c4d8cf2acdc077c45163ac7ce31bfa9
rbnhd6984 <rbnhd6984@yandex.ru>
2024-11-07 15:17:33 +0300

    Revert "refactor(autotest_py2_3.py): calculating the cost of a word in a separate function"
    This reverts commit 6d4f18609028a8ee8e52ba4db73bdaaedb3b0735.

--- autotest_py2_3.py
+++ autotest_py2_3.py
@@ -3,24 +3,18 @@
 # Будем считать, что на вход подается только одно слово, которое содержит либо только английские,
 # либо только русские буквы.
 
-def calculate_word_value(word, char_values):
-    points = 0
-    for char in word.upper():
-        for value, letters in char_values.items():
-            if char in letters:
-                points += value
-    return points
-
-chars = {
-    1: "AEIOULNSTRАВЕИНОРСТ",
-    2: "DGДКЛМПУ",
-    3: "BCMPБГЁЬЯ",
-    4: "FHVWYЙЫ",
-    5: "KЖЗХЦЧ",
-    8: "JXШЭЮ",
-    10: "QZФЩЪ"
-}
-
+chars = {1: "AEIOULNSTRАВЕИНОРСТ",
+         2: "DGДКЛМПУ",
+         3: "BCMPБГЁЬЯ",
+         4: "FHVWYЙЫ",
+         5: "KЖЗХЦЧ",
+         8: "JXШЭЮ",
+         10: "QZФЩЪ"}
+points = 0
 k = 'ноутбук'
-word_value = calculate_word_value(k, chars)
-print(word_value)
+
+for char in k:
+    for key in chars:
+        if char.upper() in chars[key]:
+            points += key
+print(points)
