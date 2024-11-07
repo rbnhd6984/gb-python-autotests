3b8fcd4831149cd28e25e58b854b51c9e53030f2
rbnhd6984 <rbnhd6984@yandex.ru>
2024-11-07 15:16:01 +0300

    Revert "refactor(autotest_py1_1.py): add random generate list coins"
    This reverts commit 88eee0491962df78d6d6c1630c6fa37a8821faa5.

--- autotest_py1_1.py
+++ autotest_py1_1.py
@@ -1,14 +1,8 @@
-# Игра орел и решка
-from random import randint, choice
 
-length = randint(1, 101)
-coins = [choice([0, 1]) for _ in range(length)]
+
+coins = [0, 1, 0, 1, 1, 0]
 heads = coins.count(0)
 tails = coins.count(1)
+answer = min(heads, tails)
 
-if heads > tails:
-    print(f'Выпало {heads} орла и {tails} решка, победил орел')
-elif heads < tails:
-    print(f'Выпало {heads} орла и {tails} решка, победил решка')
-else:
-    print(f'Выпало {heads} орла и {tails} решка, ничья')
+print(answer)
