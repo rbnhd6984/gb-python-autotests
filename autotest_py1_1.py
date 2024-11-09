bbb496173f366cc60255648b6208bdf9ca446732
rbnhd6984 <ignorth@yandex.ru>
2024-11-10 00:48:26 +0300

    refactor(autotest_py1_1.py): add random generate list coins

--- autotest_py1_1.py
+++ autotest_py1_1.py
@@ -1,8 +1,14 @@
+# Игра орел и решка
+from random import randint, choice
 
-
-coins = [0, 1, 0, 1, 1, 0]
+length = randint(1, 101)
+coins = [choice([0, 1]) for _ in range(length)]
 heads = coins.count(0)
 tails = coins.count(1)
-answer = min(heads, tails)
 
-print(answer)
+if heads > tails:
+    print(f'Выпало {heads} орла и {tails} решка, победил орел')
+elif heads < tails:
+    print(f'Выпало {heads} орла и {tails} решка, победил решка')
+else:
+    print(f'Выпало {heads} орла и {tails} решка, ничья')
