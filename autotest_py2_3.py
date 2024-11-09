a720c7ab6727316d1fc91678e332019d1e23205b
rbnhd6984 <ignorth@yandex.ru>
2024-11-10 00:50:28 +0300

    refactor(autotest_py2_3.py): calculating the cost of a word in a separate function

--- autotest_py2_3.py
+++ autotest_py2_3.py
@@ -3,18 +3,24 @@
 # Будем считать, что на вход подается только одно слово, которое содержит либо только английские,
 # либо только русские буквы.
 
-chars = {1: "AEIOULNSTRАВЕИНОРСТ",
-         2: "DGДКЛМПУ",
-         3: "BCMPБГЁЬЯ",
-         4: "FHVWYЙЫ",
-         5: "KЖЗХЦЧ",
-         8: "JXШЭЮ",
-         10: "QZФЩЪ"}
-points = 0
-k = 'ноутбук'
+def calculate_word_value(word, char_values):
+    points = 0
+    for char in word.upper():
+        for value, letters in char_values.items():
+            if char in letters:
+                points += value
+    return points
 
-for char in k:
-    for key in chars:
-        if char.upper() in chars[key]:
-            points += key
-print(points)
+chars = {
+    1: "AEIOULNSTRАВЕИНОРСТ",
+    2: "DGДКЛМПУ",
+    3: "BCMPБГЁЬЯ",
+    4: "FHVWYЙЫ",
+    5: "KЖЗХЦЧ",
+    8: "JXШЭЮ",
+    10: "QZФЩЪ"
+}
+
+k = 'ноутбук'
+word_value = calculate_word_value(k, chars)
+print(word_value)
